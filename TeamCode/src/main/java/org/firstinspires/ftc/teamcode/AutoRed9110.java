f0ae1d53a28231918535606b06f2ec95b7ccdd52
Andr3wM1ll3r <andrew.miller0810@gmail.com>
2017-09-23 16:35:14 -0400

    Added auto for red alliance 9110 9/23/2017

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed9110.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Anjew on 9/23/17.
+ */
+
+public class AutoRed9110 {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp9110.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp9110.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
 
 //wat is extens ecks dee
@@ -26,6 +27,9 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
         //Glyphs conveyor
         DcMotor conveyor;
 
+        //Distance sensor
+        public OpticalDistanceSensor distance;
+
         int Intake = 0;
         int Outtake = 0;
 
@@ -38,6 +42,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
         scissors = hardwareMap.dcMotor.get("s");
         conveyor = hardwareMap.dcMotor.get("c");
         linears = hardwareMap.dcMotor.get("li");
+        distance = hardwareMap.opticalDistanceSensor.get("d");
+
 
 
         left.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -72,7 +78,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
         }
 
 
-        //LEMON PARTY scissor lift
+        //Scissor lift
         float leftTrigger2 = gamepad2.left_trigger;
         float rightTrigger2 = gamepad2.right_trigger;
         if (leftTrigger2 < 0.05){
@@ -108,23 +114,25 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
         boolean intake = gamepad1.a;
         boolean outtake = gamepad1.x;
 
-        //Intake
-        if (intake == true && Intake == 0){
+        //Intake //sensor measured in centimeters
+        if (intake == true && Intake == 0 && distance.getLightDetected() < 0.5){
 
         Intake = 1;
+
+
         }
-        else if(intake == false && Intake == 1){
+        else if(intake == false && Intake == 1 && distance.getLightDetected() < 0.5){
 
         conveyor.setPower(0.5);
         Intake = 2;
 
         }
-        else if (intake == true && Intake == 2){
+        else if (intake == true && Intake == 2 || distance.getLightDetected() > 0.5){
 
         Intake = 3;
 
         }
-        else if (intake == false && Intake == 3){
+        else if (intake == false && Intake == 3 || distance.getLightDetected() > 0.5){
 
         conveyor.setPower(0.0);
         Intake = 0;
@@ -150,6 +158,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
         Outtake = 0;
         }
 
-        }
 
         }
+
+
+}
